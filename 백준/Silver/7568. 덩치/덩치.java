ba78206d7258dd57cacfa7a53ba438c871f47f9d
dongguk-whmark <155556447+dongguk-whmark@users.noreply.github.com>
2024-02-04 21:29:10 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] person = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            person[i][0] = Integer.parseInt(st.nextToken());
+            person[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+            for (int j = 0; j < N; j++) {
+                if(i == j) continue;
+
+                if(person[i][0] < person[j][0] && person[i][1] < person[j][1]){
+                    rank++;
+                }
+            }
+
+            sb.append(rank).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
