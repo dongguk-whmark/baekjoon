4385ca4e7e9dc39b28e09d1ec83e3da7fe4d644f
dongguk-whmark <155556447+dongguk-whmark@users.noreply.github.com>
2024-03-01 00:18:50 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 736 ms, Memory: 51060 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,3 +1,6 @@
+// 백준 14940 쉬운 최단거리. 자바 풀이
+// 2024-03-01 12:11
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,6 +14,7 @@ public class Main {
     static int[] dx = {0,0,-1,1};
     static int[] dy = {-1,1,0,0};
     static int[][] map;
+    static int[][] result;
     static boolean[][] visited;
     static Queue<int[]> q = new LinkedList<>();
 
@@ -23,6 +27,7 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
+        result = new int[n][m];
         visited = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
@@ -39,20 +44,17 @@ public class Main {
                     x = i;
                     y = j;
                 }
-                else if (map[i][j] == 0){
-                    visited[i][j] = true;
+                else if (map[i][j] == 1){
+                    result[i][j] = -1;
                 }
             }
         }
-        map[x][y] = 0;
+        result[x][y] = 0;
         bfs(x,y);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!visited[i][j]) {
-                    map[i][j] = -1;
-                }
-                sb.append(map[i][j]).append(" ");
+                sb.append(result[i][j]).append(" ");
             }
             sb.append('\n');
         }
@@ -72,7 +74,7 @@ public class Main {
                 if(nx >= 0 && ny >= 0 && nx < n && ny < m){
                     if(!visited[nx][ny] && map[nx][ny] == 1){
                         visited[nx][ny] = true;
-                        map[nx][ny] = map[cur[0]][cur[1]] + 1;
+                        result[nx][ny] = result[cur[0]][cur[1]] + 1;
                         q.add(new int[] {nx,ny});
                     }
                 }
