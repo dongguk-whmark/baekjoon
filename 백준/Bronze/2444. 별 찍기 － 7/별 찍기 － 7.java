017ffb6a360e73c5f1dcce0f453f96b3c6ef0cf5
dongguk-whmark <155556447+dongguk-whmark@users.noreply.github.com>
2024-03-18 01:06:21 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < N-i; j++) {
+                sb.append(' ');
+            }
+            for (int j = 0; j < 2*i - 1; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+
+        for (int i = 0; i < 2*N - 1; i++) {
+            sb.append('*');
+        }
+        sb.append('\n');
+
+        for (int i = N-1; i >= 1; i--) {
+            for (int j = 0; j < N-i; j++) {
+                sb.append(' ');
+            }
+            for (int j = 0; j < 2*i - 1; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
